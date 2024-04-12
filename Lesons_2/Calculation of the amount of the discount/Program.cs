919705699098255266ffbdf4942e9a638113cd47
user <user@WIN-H2QPH3A41C1>
2024-04-12 17:12:44 +0300

    Task_2.1

---
+++ Lesons_2/Calculation of the amount of the discount/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculation_of_the_amount_of_the_discount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int price =369;
+            int discount = 18;
+            int reducedPrice =price-((price / 100) *  discount);
+                Console.WriteLine($"Price:{price}");
+            Console.WriteLine($"Discount:{discount}");
+            Console.WriteLine($"Reduced prsce:{reducedPrice}");
+            Console.ReadLine();
+        }
+    }
+}
