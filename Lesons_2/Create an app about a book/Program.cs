9345bd10b1fc8dde93db68f5137938cbffea8695
user <user@WIN-H2QPH3A41C1>
2024-04-13 16:49:29 +0300

    Create an app about a book

---
+++ Lesons_2/Create an app about a book/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Create_an_app_about_a_book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
