7b90393c29310f8674a208a7620c3599509f1448
user <user@WIN-H2QPH3A41C1>
2024-04-13 15:39:59 +0300

    conversion to seconds

--- Lesons_2/Time_in_second/Program.cs
+++ Lesons_2/Time_in_second/Program.cs
@@ -8,6 +8,11 @@
             int houe=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("drive an minutes:");
             int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("drive an seconds");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            int justSeconds =( seconds + (minutes * 60)+(houe * (60*60)));
+            Console.WriteLine("Just seconds:" + Convert.ToInt32(justSeconds));
+           
             Console.ReadLine();
         }
     }
