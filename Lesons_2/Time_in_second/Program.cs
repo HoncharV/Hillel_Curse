62ca8b07066f85950e5a7d090ee13efb2b8481f5
user <user@WIN-H2QPH3A41C1>
2024-04-13 13:24:05 +0300

    Task_2.2

---
+++ Lesons_2/Time_in_second/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Time_in_second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("drive an hour:");
+            int houe=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("drive an minutes:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
