3f782d580ab42b29a8163361556bf8523e8bf565
user <user@WIN-H2QPH3A41C1>
2024-04-14 13:54:33 +0300

    square root

---
+++ Lesons_3/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            //2) Написати програму,
+            //яка обчислює квадрат
+            //будь - якого введеного числа(бажано використати Math).
+            Console.WriteLine("enter a number:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Math.BigMul.SquareRoot(number));
+            //Console.WriteLine(number);
+            int squareRoot = number * number;
+            Console.WriteLine("Square root = "+ squareRoot);
+            Console.ReadKey();
+        }
+    }
+}
