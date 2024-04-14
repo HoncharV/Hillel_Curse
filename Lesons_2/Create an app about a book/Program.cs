693e3871107ce8e4aee625fcbaa6060a8ebdc556
user <user@WIN-H2QPH3A41C1>
2024-04-14 11:49:58 +0300

    The title of the book

--- Lesons_2/Create an app about a book/Program.cs
+++ Lesons_2/Create an app about a book/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Створіть програму,
+            //яка виводить інформацію про книгу,
+            //використовуючи форматований рядок.
+            //Параметри книги -назва,
+            //автор, рік та ціна.
+            string name="Head First";
+            string author="Andrew Stillman, Jennifer Green";
+            string year = "2022";
+            int price = 690;/*ціна в гривнях*/
+            Console.WriteLine($"The title of the book:{name}");
+            Console.WriteLine($"Author: {author}");
+            Console.WriteLine($"Year:{year}");
+            Console.WriteLine("Price:"+Convert.ToInt32(price));
+            Console.ReadLine();
+
         }
     }
 }
