611ff52137377bd22b9df767a281e2e4993a59d4
user <user@WIN-H2QPH3A41C1>
2024-04-14 12:51:05 +0300

    A three-digit number is given

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Дано тризначне число.
+            //Знайти число,
+            //отримане під час прочитання його цифр справа наліво.
+            Console.WriteLine("enter a three-digit number :");
+            int numeric=Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
