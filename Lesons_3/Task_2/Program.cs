356b22e30bbd8dc582dc8ee0958ce413c7df8e76
user <user@WIN-H2QPH3A41C1>
2024-04-14 14:40:02 +0300

    Square root

--- Lesons_3/Task_2/Program.cs
+++ Lesons_3/Task_2/Program.cs
@@ -8,12 +8,15 @@
             //2) Написати програму,
             //яка обчислює квадрат
             //будь - якого введеного числа(бажано використати Math).
-            Console.WriteLine("enter a number:");
-            int number=Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(Math.BigMul.SquareRoot(number));
-            //Console.WriteLine(number);
-            int squareRoot = number * number;
-            Console.WriteLine("Square root = "+ squareRoot);
+            //Console.WriteLine("enter a number:");
+            //int number=Convert.ToInt32(Console.ReadLine());
+            //int squareRoot = number * number;
+            //Console.WriteLine("Square root = "+ squareRoot);
+            // варіат_2 з методом marh
+            Console.WriteLine("re-enter the number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s = Math.Pow(a,2);
+            Console.WriteLine("Square root = " + s);
             Console.ReadKey();
         }
     }
