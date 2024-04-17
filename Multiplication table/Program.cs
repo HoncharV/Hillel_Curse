c7027bf8210d899bdfd26104cb32779b7147dd56
user <user@WIN-H2QPH3A41C1>
2024-04-17 22:47:32 +0300

    multiplication table

---
+++ Multiplication table/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Multiplication_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Таблиця множення
+            for (int i = 1;i <= 10; i++) // 1*1=1
+            {
+               for (int j = 1; j <=10; j++)
+               {
+                   Console.WriteLine($"{i} * {j} = {i*j}");
+               }
+            }
+        }
+    }
+}
