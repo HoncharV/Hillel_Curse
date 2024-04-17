1e63e100cef91ca35ab67328bdfbc1fdb3e87cf5
user <user@WIN-H2QPH3A41C1>
2024-04-17 21:45:06 +0300

    Write a program that will "ask" for the correct password

---
+++ Lesons_5/Password/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Напишіть програму, яка «запитуватиме» правильний пароль,
+            //доки він не буде введений. Правильний пароль нехай буде "root".
+            //Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!
+            string p;
+            do
+            {
+                Console.WriteLine("enter password!!!");
+              p=Console.ReadLine();
+                Console.WriteLine("The password is incorrect, please enter it again");
+            }
+            while (p!= "root");
+        }
+    }
+} 
