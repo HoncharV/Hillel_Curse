fc7038495eb646be4264500b001076c73edfbc56
user <user@WIN-H2QPH3A41C1>
2024-04-17 22:15:45 +0300

    The program guesses the number from 1 to 146

---
+++ Lesons_5/hello Random/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace hello_Random
+{
+    internal class Program
+    {
+        static void Main(string[] arge)
+        {            //2) Програма загадує число від 1 до 146(привіт, Random).
+                     //Користувач намагається вгадати його.
+                     //У разі неправильної відповіді програма підказує «більше» або «менше»
+            Random random = new Random();
+
+            int min = 1;
+            int max = 146;
+            int randomNumber = random.Next(min, max);
+            int p;
+
+            do
+            {
+                Console.WriteLine("guess the number");
+                p = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The password is incorrect, please enter it again");
+                if (p < randomNumber) Console.WriteLine("more");
+                if (p > randomNumber) Console.WriteLine("Less");
+            }
+            while (p != randomNumber);
+        }
+    }
+}
