ab0c4df3afc0473435fe254471693423a26ac57b
user <user@WIN-H2QPH3A41C1>
2024-04-22 15:26:29 +0300

    macros

--- Lesons_6/Massif/Program.cs
+++ Lesons_6/Massif/Program.cs
@@ -8,26 +8,30 @@
             //Заповнити цей масив рандомними числами від -100 до 100.
             //Знайти кількість додатніх чисел у масиві.
             //Вивести на екран масив і кількість порахованих чисел.
+
+            //int n = 10;
+            //int[] a = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < n; i++) // генерування масиву випадковими числами
+            //    a[i] = random.Next(-100,100);
+            //Console.WriteLine("Result:");//виведення елементів даного масиву
+
+            //{
+
+            //}
+            int l = 10;
+            int[] a = new int[l];
             
-            int n = 10;
-            int[] a = new int[n];
-            Random rand = new Random();
-            for (int i = 0; i < n; i++)// генерування масиву випадковими числами
-            a[i] = rand.Next(-100, 100);
-            Console.WriteLine("Результати:");//виведення елементів даного масиву,
-                                             //та подвоєні елементи
-            Console.WriteLine("#  елемент  ");
-
-            for (int i = 0; i < n; i++)
-
-
-
+            //Console.WriteLine(a.Length);
+            Random random = new Random();
+            int i = 0;
+            for (; i < l; i++)
             {
-
-                Console.WriteLine(a[i]);
-
+                a[i] = random.Next(-100,100);
+                Console.WriteLine(a[i]);    
             }
-            Console.ReadLine();
+            
+                Console.ReadLine();
         }
     }
 }
