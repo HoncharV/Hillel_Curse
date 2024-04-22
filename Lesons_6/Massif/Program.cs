5ac2491761801ab0d61aedcaa5bb6d4523761e3a
user <user@WIN-H2QPH3A41C1>
2024-04-22 14:01:10 +0300

    Specify an array of 10 elements.

---
+++ Lesons_6/Massif/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Massif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Задати масив із 10 елементів.
+            //Заповнити цей масив рандомними числами від -100 до 100.
+            //Знайти кількість додатніх чисел у масиві.
+            //Вивести на екран масив і кількість порахованих чисел.
+            
+            int n = 10;
+            int[] a = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)// генерування масиву випадковими числами
+            a[i] = rand.Next(-100, 100);
+            Console.WriteLine("Результати:");//виведення елементів даного масиву,
+                                             //та подвоєні елементи
+            Console.WriteLine("#  елемент  ");
+
+            for (int i = 0; i < n; i++)
+
+
+
+            {
+
+                Console.WriteLine(a[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
